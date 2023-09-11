027c0b0e810d88268ad22ff13a6a4630fa8606a4
denyncrawford <kiquerangelb@gmail.com>
2023-09-11 12:04:06 -0300

    feat: cache

--- services/api.service.ts
+++ services/api.service.ts
@@ -1,11 +1,18 @@
+import axios, { getAdapter } from "axios";
+import {
+  cacheAdapterEnhancer,
+  throttleAdapterEnhancer,
+} from "axios-extensions";
 
-import axios from "axios";
-
-const GITHUB_AUTH_TOKEN = Deno.env.get('GITHUB_AUTH_TOKEN') || '';
+const GITHUB_AUTH_TOKEN = Deno.env.get("GITHUB_AUTH_TOKEN") || "";
 
 export const api = axios.create({
-  baseURL: 'https://api.github.com',
+  baseURL: "https://api.github.com",
   headers: {
     Authorization: `Bearer ${GITHUB_AUTH_TOKEN}`,
-  }
-})
+    "Cache-Control": "no-cache",
+  },
+  adapter: throttleAdapterEnhancer(
+    cacheAdapterEnhancer(getAdapter(axios.defaults.adapter)),
+  ),
+});
