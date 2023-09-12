27201035ad8babd8f57049a11dded419cd593bfd
denyncrawford <kiquerangelb@gmail.com>
2023-09-12 02:01:44 -0300

    feat: enhance cache

--- services/api.service.ts
+++ services/api.service.ts
@@ -1,10 +1,15 @@
-import axios, { getAdapter } from "axios";
+import axios, { AxiosPromise, getAdapter } from "axios";
+import { LRUCache } from 'https://esm.sh/lru-cache@10.0.1'
 import {
+  ICacheLike,
   cacheAdapterEnhancer,
   throttleAdapterEnhancer,
 } from "axios-extensions";
+import { IProject } from "@/services/projects.types.ts";
 
 const GITHUB_AUTH_TOKEN = Deno.env.get("GITHUB_AUTH_TOKEN") || "";
+
+export const cache = new LRUCache({ ttl: 24 * 60 * 60 * 1000, max: 3 });
 
 export const api = axios.create({
   baseURL: "https://api.github.com",
@@ -13,6 +18,8 @@ export const api = axios.create({
     "Cache-Control": "no-cache",
   },
   adapter: throttleAdapterEnhancer(
-    cacheAdapterEnhancer(getAdapter(axios.defaults.adapter)),
+    cacheAdapterEnhancer(getAdapter(axios.defaults.adapter), {
+      defaultCache: cache as ICacheLike<AxiosPromise<IProject>>,
+    }),
   ),
 });
